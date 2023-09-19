f9804aa807c77ee33f59238c4a18d4b850d098c5
Pietro Vallardi Grohe <pietrovallardigrohe@gmail.com>
2023-09-19 10:52:04 -0300

    clear_terminal function created

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+pub fn clear_terminal() -> i32 {
+    use std::process::Command;
+    let exit_code = if cfg!(windows) {
+        Command::new("cmd")
+            .args(["/c", "cls"])
+            .spawn()
+            .expect("cls command failed to start")
+            .wait()
+                
+    } else {
+        Command::new("clear")
+            .spawn()
+            .expect("clear command failed to start")
+            .wait()
+    };
+
+    match exit_code {
+        Ok(exit_status) => {
+            println!("{:?}", exit_status.code().expect("Exited by signal"));
+            exit_status.code().expect("Exited by signal")
+        },
+        Err(_) => {
+            println!("failed to wait");
+            -1
+        }
+    }
+    // println!("{:?}", exit_status);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn clear_terminal_test() {
+        assert!(clear_terminal() == 0);
+        // assert_eq!(result, 4);
+    }
+}
